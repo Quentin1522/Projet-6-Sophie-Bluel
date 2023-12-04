af2ce593a98998eeb9d10a0a25331366fcdd94cb
Quentin Honreck <horneck.quentin@gmail.com>
2023-12-04 19:01:27 +0100

    upgrade

---
+++ FrontEnd/assets/index.js
@@ -0,0 +1,91 @@
+// Récupération des éléments du DOM
+const gallery = document.getElementById("gallery");
+const filters = document.querySelector(".filters");
+
+// Fonction asynchrone pour effectuer une requête API et récupérer les œuvres
+const getApi = async () =>
+  await (await fetch("http://localhost:5678/api/works")).json();
+
+// Fonction asynchrone pour effectuer une requête API et récupérer les catégories
+const getCategories = async () =>
+  await (await fetch("http://localhost:5678/api/categories")).json();
+
+// Fonction pour créer un élément dans la galerie
+const displayApi = (api) => {
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figcaption = document.createElement("figcaption");
+
+  // Attribution des valeurs à chaque élément
+  img.src = api.imageUrl;
+  figcaption.textContent = api.title;
+
+  // Ajout des éléments à la figure
+  figure.classList.add("itemGallery");
+  figure.append(img, figcaption);
+
+  // Ajout de la figure à la galerie
+  gallery.appendChild(figure);
+};
+
+// Fonction pour afficher les œuvres dans la galerie
+const displayApis = (apis) => {
+  // Vide la galerie avant d'y ajouter de nouveaux éléments
+  gallery.innerHTML = "";
+  // Appelle la fonction displayApi pour chaque œuvre
+  apis.forEach(displayApi);
+};
+
+// Fonction asynchrone pour créer les boutons de catégorie
+const createCategoryButtons = async () => {
+  // Récupération des catégories depuis l'API
+  const categories = await getCategories();
+
+  // Pour chaque catégorie, créer un bouton et l'ajouter aux filtres
+  categories.forEach((category) => {
+    const btn = document.createElement("button");
+    btn.textContent = category.name.toUpperCase();
+    btn.id = category.id;
+    filters.appendChild(btn);
+  });
+};
+
+// Fonction asynchrone pour filtrer les œuvres par catégorie
+const filterByCategory = async () => {
+  // Récupération de toutes les œuvres depuis l'API
+  const allGallery = await getApi();
+  // Sélection de tous les boutons de filtre
+  const buttons = document.querySelectorAll(".filters button");
+
+  // Pour chaque bouton de filtre, ajouter un écouteur d'événement de clic
+  buttons.forEach((button) => {
+    button.addEventListener("click", async (e) => {
+      // Récupération de l'ID de la catégorie depuis le bouton cliqué
+      const categoryId = e.target.id;
+
+      // Filtrer les œuvres en fonction de la catégorie sélectionnée
+      //categoryId différents de 0
+      const filteredGallery =
+        categoryId !== "0"
+          ? allGallery.filter((api) => api.categoryId == categoryId)
+          : allGallery;
+
+      // Afficher les œuvres filtrées dans la galerie
+      displayApis(filteredGallery);
+    });
+  });
+};
+
+// Fonction asynchrone pour initialiser la galerie
+const initializeGallery = async () => {
+  // Créer les boutons de catégorie
+  await createCategoryButtons();
+  // Ajouter les écouteurs d'événements pour les filtres
+  await filterByCategory();
+  // Récupérer toutes les œuvres et les afficher dans la galerie
+  const allGallery = await getApi();
+  displayApis(allGallery);
+};
+
+// Appel de la fonction d'initialisation de la galerie
+initializeGallery();
---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,75 @@
+// Modèle de vérification d'une adresse e-mail plus général
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/i;
+
+// Fonction pour valider l'email et le mot de passe
+function validerEmailEtMotDePasse(email, password, motDePasseResponse) {
+  // Validation de l'email
+  const emailValide = emailRegex.test(email);
+
+  // Validation du mot de passe en comparant avec celui de la réponse
+  const motDePasseValide = password === motDePasseResponse;
+
+  // Retourne true si l'email et le mot de passe sont valides, sinon false
+  return emailValide && motDePasseValide;
+}
+
+// Récupération des éléments du formulaire
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("mdp");
+const form = document.querySelector("form");
+
+// Gestionnaire d'événement lors de la soumission du formulaire
+form.addEventListener("submit", async (event) => {
+  // Empêche le comportement par défaut (rechargement de la page)
+  event.preventDefault();
+
+  // Récupération des valeurs des champs
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  try {
+    // Effectuer la requête pour obtenir le token
+    const response = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    // Vérifier si la requête a réussi
+    if (response.ok) {
+      // Récupérer le token à partir de la réponse JSON
+      const data = await response.json();
+      const token = data.token;
+      const motDePasseResponse = data.password;
+
+      // Vérifier l'email et le mot de passe
+      if (validerEmailEtMotDePasse(email, password, motDePasseResponse)) {
+        // Redirection vers "modif.html" si la validation réussit
+        window.location.href = "modif.html";
+        // Enregistrer le token dans sessionStorage
+        window.sessionStorage.setItem("token", token);
+      } else {
+        console.log(
+          "La validation a échoué. Veuillez vérifier vos informations."
+        );
+        alert("La validation a échoué. Veuillez vérifier vos informations.");
+      }
+    } else {
+      // Afficher un message d'erreur en cas d'échec de la requête
+      console.log("Erreur lors de la requête :", response.status);
+      alert("Erreur lors de la requête. Veuillez réessayer.");
+    }
+  } catch (error) {
+    console.error("Erreur inattendue :", error);
+    alert("Une erreur inattendue s'est produite. Veuillez réessayer.");
+  }
+});
+
+// Fonction pour se déconnecter
+function logout() {
+  localStorage.removeItem("token");
+  window.location.href = "../index.html";
+}
+
+// Appel de la fonction de déconnexion
+logout();
