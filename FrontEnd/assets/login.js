b57a388c8f92ea1c52655a5825fcee01b7297ebb
Quentin Honreck <horneck.quentin@gmail.com>
2023-12-20 15:58:14 +0100

    maj

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,117 +1,196 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Récupérer les références des éléments HTML
-  const email = document.getElementById("email");
-  const password = document.getElementById("mdp");
-  const form = document.querySelector("form");
-  const logoutBtn = document.getElementById("logoutBtn");
+document.addEventListener("DOMContentLoaded", function () {
+  // Récupérer les références des éléments HTML qui dépendent de l'état de connexion
+  const editButton = document.getElementById("editButton"); // Bouton de modification
+  const iconSvg = document.getElementById("iconSvg"); // Icône SVG
+  const navList = document.querySelector("nav ul"); // Liste de navigation où les liens sont ajoutés
 
   // Fonction de gestionnaire de redirection vers la page login.html
   function redirectToLoginPage() {
-    // Redirection vers la page login.html
     window.location.href = "assets/login.html";
-  }
-
-  // Fonction pour mettre à jour le contenu et le style du bouton en fonction de l'état de connexion
-  function updateLoginButton() {
-    const loginButton = document.getElementById("loginButton");
-
-    if (isLoggedIn()) {
-      console.log("User is logged in");
-      loginButton.textContent = "logout";
-      // Ajoutez des styles ou des classes CSS pour le bouton de déconnexion si nécessaire
-    } else {
-      console.log("User is not logged in");
-      loginButton.textContent = "login";
-      // Ajoutez des styles ou des classes CSS pour le bouton de connexion
-    }
-  }
-
-  // Fonction pour créer et ajouter dynamiquement le bouton "login" dans le menu
-  function createLoginButton() {
-    // Créez l'élément du bouton
-    const loginButton = document.createElement("li");
-    loginButton.id = "loginButton";
-    loginButton.textContent = "login";
-    loginButton.addEventListener("click", redirectToLoginPage);
-
-    // Récupérez l'élément de la liste de navigation
-    const nav = document.querySelector("nav ul");
-
-    // Récupérez les deux premiers éléments <li>
-    const firstLi = nav.children[0];
-    const secondLi = nav.children[1];
-
-    // Ajoutez le bouton après les deux premiers éléments <li>
-    nav.insertBefore(loginButton, secondLi.nextSibling);
-
-    // Mettez à jour le contenu et le style du bouton en fonction de l'état de connexion initial
-    updateLoginButton();
-  }
-
-  // Fonction pour vérifier l'état de connexion
-  function isLoggedIn() {
-    // Vérifiez si un token est présent dans le localStorage
-    const token = localStorage.getItem("token");
-    return !!token; // Renvoie true si le token existe, sinon false
-  }
-
-  // Gestionnaire de soumission du formulaire de connexion
-  async function handleLoginSubmit(e) {
-    e.preventDefault(); // Empêcher la soumission par défaut
-
-    // Récupération des valeurs des champs
-    const userEmail = email.value;
-    const userPassword = password.value;
-
-    try {
-      // Effectuer la requête pour obtenir le token
-      const response = await fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email: userEmail, password: userPassword }),
-      });
-
-      // Vérifier si la requête a réussi
-      if (response.ok) {
-        // Récupérer le token à partir de la réponse JSON
-        const data = await response.json();
-        const token = data.token;
-
-        window.location.href = "../index.html";
-
-        // Enregistrer le token dans le localStorage
-        localStorage.setItem("token", token);
-      } else {
-        // Afficher un message d'erreur en cas d'échec de la requête
-        console.log("Erreur lors de la requête :", response.status);
-        alert("Identifiants incorrects. Veuillez réessayer.");
-      }
-    } catch (error) {
-      console.error("Erreur inattendue :", error);
-      alert("Une erreur inattendue s'est produite. Veuillez réessayer.");
-    }
-  }
-
-  // Appel initial pour créer le bouton au chargement de la page
-  createLoginButton();
-  updateLoginButton(); // Mettez à jour le bouton lors du chargement de la page
-
-  //Déconnexion//////////////////////////////////////////////////////////////
-  // Événement au clic sur le bouton de déconnexion
-  if (logoutBtn) {
-    logoutBtn.addEventListener("click", logout);
-  } else {
-    console.error("Erreur : Le bouton de déconnexion n'a pas été trouvé.");
   }
 
   // Fonction de déconnexion
   function logout() {
     localStorage.removeItem("token");
-    window.location.href = "../index.html";
+    updateUI();
+    redirectToHomePage(true);
   }
 
-  const loginForm = document.getElementById("login-form");
+  // Redirection vers la page d'accueil après déconnexion
+  function redirectToHomePage(isLogout) {
+    if (isLogout) window.location.href = "index.html";
+    else window.location.href = "../index.html";
+  }
+
+  function redirectToLoginPage() {
+    window.location.href = "assets/login.html";
+  }
+
+  // Fonction pour vérifier si l'utilsateur est connecté
+  function isLoggedIn() {
+    return !!localStorage.getItem("token");
+  }
+
+  function updateUI() {
+    const loginLI = document.getElementById("loginLI"); // L'élément <li> pour le bouton de connexion/déconnexion
+
+    if (isLoggedIn()) {
+      loginLI.textContent = "logout";
+      loginLI.removeEventListener("click", redirectToLoginPage);
+      loginLI.addEventListener("click", logout);
+      // Afficher le bouton de modification et l'icône
+      if (editButton) editButton.style.display = "inline";
+      if (iconSvg) iconSvg.style.display = "inline";
+    } else {
+      // Modifier le texte pour afficher "Login"
+      loginLI.textContent = "login";
+      loginLI.removeEventListener("click", logout);
+      loginLI.addEventListener("click", redirectToLoginPage);
+
+      // Masquer le bouton de modification et l'icône
+      if (editButton) editButton.style.display = "none";
+      if (iconSvg) iconSvg.style.display = "none";
+    }
+  }
+
+  // Créer le bouton login/logout et l'ajouter à la navigation
+  function createLoginLogoutButton() {
+    const loginLI = document.createElement("li");
+    loginLI.id = "loginLI";
+
+    // Insérer le bouton avant le dernier élément de 'navList' si 'navList' a plus d'un enfant
+    if (navList.children.length > 1) {
+      // L'avant-dernier emplacement est juste avant le dernier enfant
+      const lastLi = navList.lastElementChild;
+      navList.insertBefore(loginLI, lastLi);
+    } else {
+      // S'il n'y a qu'un seul enfant ou aucun, simplement ajouter à la fin
+      navList.append(loginLI);
+    }
+
+    updateUI(); // Mettre à jour l'état immédiatement après la création
+  }
+
+  // Gestionnaire d'événement pour la soumission du formulaire
+  function handleLoginSubmit(event) {
+    event.preventDefault();
+    const emailInput = document.getElementById("email"); // Assurez-vous que l'ID est correct dans le HTML
+    const passwordInput = document.getElementById("password"); // Assurez-vous que l'ID est correct dans le HTML
+
+    const email = emailInput.value.trim(); // Ligne susceptible de générer l'erreur
+    const password = passwordInput.value.trim(); // Ligne susceptible de générer l'erreur
+
+    fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("token", data.token);
+        updateUI();
+        redirectToHomePage(false);
+      })
+      .catch((error) => {
+        console.error("Login failed:", error);
+        alert("Login failed. Please try again.");
+      });
+  }
+
+  // Attacher l'événement de soumission au formulaire de connexion, s'il est présent
+  const loginForm = document.getElementById("loginForm");
   if (loginForm) {
     loginForm.addEventListener("submit", handleLoginSubmit);
   }
+
+  // Initialiser le bouton de connexion/déconnexion à partir de l'état de stockage local
+  createLoginLogoutButton();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Récupération de l'état de connexion
+  const updateAuthenticationState = () => {
+    const isAuthenticated = isLoggedIn();
+    displayEditOptions(isAuthenticated);
+    displayAuthenticationButton(isAuthenticated);
+  };
+
+  const displayEditOptions = (isAuthenticated) => {
+    const editElements = document.getElementsByClassName("edit");
+    for (const element of editElements) {
+      element.style.display = isAuthenticated ? "inline" : "none";
+    }
+  };
+
+  const displayAuthenticationButton = (isAuthenticated) => {
+    const loginButton = document.getElementById("loginButton");
+    const logoutButton = document.getElementById("logoutButton");
+
+    if (isAuthenticated) {
+      loginButton.style.display = "none";
+      logoutButton.style.display = "inline";
+    } else {
+      loginButton.style.display = "inline";
+      logoutButton.style.display = "none";
+    }
+  };
+
+  const isLoggedIn = () => {
+    return !!localStorage.getItem("token");
+  };
+
+  const loginForm = document.getElementById("loginForm");
+  const emailInput = document.getElementById("email");
+  const passwordInput = document.getElementById("password");
+
+  // Écouteur d'événement pour la soumission du formulaire de connexion
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const email = email.value;
+    const password = passwordInput.value;
+
+    // Supposons que l'API retourne un objet avec un token en cas de succès
+    fetch("http://localhost:5678/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok " + response.statusText);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Sauvegarde du token dans le localStorage
+        localStorage.setItem("token", data.token);
+        updateAuthenticationState();
+        // Redirection vers la page principale
+        window.location.href = "index.html";
+      })
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+      });
+  });
+
+  // Écouteur d'événement pour le bouton de déconnexion
+  document.getElementById("logoutButton").addEventListener("click", () => {
+    localStorage.removeItem("token");
+    updateAuthenticationState();
+    // Redirection vers la page de connexion
+    window.location.href = "login.html";
+  });
+
+  // Vérification initiale de l'état de connexion
+  updateAuthenticationState();
 });
--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -87,9 +87,6 @@ const initializeGallery = async () => {
   displayApis(allGallery);
 };
 
-// Appel de la fonction
-initializeGallery();
-
 // Sélectionnez l'élément parent
 const modifyContainer = document.querySelector(".modify");
 
@@ -157,3 +154,182 @@ imageInput.addEventListener("input", () => {
     validerButton.style.color = "white";
   }
 });
+
+// Ajout de l'état de connexion pour montrer/masquer les boutons de modification
+const isLoggedIn = () => localStorage.getItem("token") !== null;
+
+const updateModifyElements = () => {
+  const modifyElements = document.querySelectorAll(".modify-item");
+  modifyElements.forEach((elem) => {
+    elem.style.visibility = isLoggedIn() ? "visible" : "hidden";
+  });
+};
+
+// Initialisation de la galerie
+document.addEventListener("DOMContentLoaded", async () => {
+  await initializeGallery();
+  updateModifyElements();
+});
+
+//Modal
+// Sélection des éléments DOM
+const modalWrapper = document.querySelector(".modal-wrapper");
+const modalTriggers = document.querySelectorAll(".modal-trigger");
+const galleryModal = document.getElementById("gallery-modal");
+const wrapperPhoto = document.querySelector(".wrapperPhoto");
+const ajoutBtn = document.querySelector(".ajout");
+const ajoutFile = document.querySelector(".ajout-file");
+const photoTriggers = document.querySelectorAll(".photo-trigger");
+const closePhotoBtn = document.querySelector(".close-photo");
+const arrowBtn = document.querySelector(".arrow");
+const formPhoto = document.querySelector(".form-photo");
+
+let works = []; // Déclarez la variable works en dehors des fonctions
+
+// Fonction pour basculer l'état du modal principal
+function toggleModal(e) {
+  e.preventDefault();
+  modalWrapper.classList.toggle("active");
+}
+
+// Fonction pour basculer l'état de la galerie photo
+function toggleGalleryModal(e) {
+  galleryModal.classList.toggle("active");
+}
+
+// Fonction pour basculer l'état de l'ajout de photo
+function toggleAddPhotoModal(e) {
+  wrapperPhoto.classList.toggle("active");
+}
+
+// Attacher les gestionnaires d'événements pour les déclencheurs modaux principaux
+modalTriggers.forEach((trigger) => {
+  trigger.addEventListener("click", toggleModal);
+});
+
+// Attacher les gestionnaires d'événements pour le bouton "Gallerie photo"
+photoTriggers.forEach((trigger) => {
+  trigger.addEventListener("click", toggleGalleryModal);
+});
+
+// Attacher un gestionnaire d'événements pour le bouton "Ajouter une photo"
+ajoutBtn.addEventListener("click", toggleAddPhotoModal);
+
+// Attacher un gestionnaire d'événements pour la croix de la deuxième modal
+closePhotoBtn.addEventListener("click", toggleAddPhotoModal);
+
+// Ajouter un gestionnaire d'événements à la flèche dans la deuxième modal
+arrowBtn.addEventListener("click", (e) => {
+  if (e) {
+    e.preventDefault();
+  }
+  toggleAddPhotoModal();
+  toggleGalleryModal();
+});
+
+// Fonction asynchrone pour récupérer les œuvres depuis l'API dans la modal
+const fetchWorks = async () => {
+  try {
+    const response = await fetch("http://localhost:5678/api/works");
+    if (!response.ok) {
+      throw new Error(
+        `Erreur de chargement des œuvres. Code HTTP : ${response.status}`
+      );
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(
+      "Erreur lors de la récupération des données de l'API :",
+      error
+    );
+    throw error;
+  }
+};
+
+// Fonction pour créer un élément dans la galerie
+const createGalleryItem = (work) => {
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+
+  // Ajouter les données récupérées à la balise img
+  img.src = work.imageUrl;
+  img.alt = work.title;
+
+  figure.classList.add("itemGallery");
+  figure.appendChild(img);
+
+  // Ajouter un événement pour afficher les détails au clic sur l'image
+  figure.addEventListener("click", () => {});
+  return figure;
+};
+
+// Fonction pour afficher les œuvres dans la galerie
+const displayWorks = (works) => {
+  console.log(works);
+  galleryModal.innerHTML = ""; // Vide la galerie
+  works.forEach((work) => {
+    const galleryItem = createGalleryItem(work);
+    galleryModal.appendChild(galleryItem);
+  });
+};
+
+// Fonction asynchrone pour initialiser la galerie
+const initializeGalleryModal = async () => {
+  try {
+    works = await fetchWorks();
+    displayWorks(works);
+  } catch (error) {
+    console.error("Erreur lors de l'initialisation de la galerie :", error);
+  }
+};
+
+// Appeler la fonction d'initialisation de la galerie
+document.addEventListener("DOMContentLoaded", initializeGalleryModal);
+
+// Fonction pour ajouter un nouveau projet
+const submitNewWork = async (formData) => {
+  try {
+    const response = await fetch("http://localhost:5678/api/works", {
+      method: "POST",
+      body: formData,
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    });
+    if (!response.ok) {
+      throw new Error(
+        `Erreur lors de l'ajout du projet. Code HTTP : ${response.status}`
+      );
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Erreur lors de l'ajout du projet à l'API :", error);
+    throw error;
+  }
+};
+
+// Fonction pour gérer la soumission du formulaire
+const handleFormSubmit = async (e) => {
+  e.preventDefault();
+
+  // Créer un objet FormData basé sur les champs du formulaire
+  const formData = new FormData(formPhoto);
+
+  try {
+    // Soumission du formulaire et récupération des données du projet ajouté
+    const newWork = await submitNewWork(formData);
+
+    // Ajout à la liste des œuvres en mémoire et rafraichissement de la galerie
+    works.push(newWork);
+    displayWorks(works);
+
+    // Ferme la boîte modale de l'ajout de photo
+    toggleAddPhotoModal();
+  } catch (error) {
+    console.error("Erreur lors de la gestion du formulaire :", error);
+  }
+};
+
+// Attache le gestionnaire d'événements de soumission du formulaire
+formPhoto.addEventListener("submit", handleFormSubmit);
